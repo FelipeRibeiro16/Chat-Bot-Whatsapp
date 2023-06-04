e70b93ba7ddcc0e33dcafb4b73e8b653c9d4351c
FelipeRibeiro16 <96789772+FelipeRibeiro16@users.noreply.github.com>
2023-06-04 18:08:26 -0300

    Update whatsapp.py for docker deploy

--- bot_modules/whatsapp.py
+++ bot_modules/whatsapp.py
@@ -49,6 +49,7 @@ class WhatsApp:
         """
         chrome_options = Options()
         chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("window-size=1080,720")
         chrome_options.add_argument("--start-maximized")
         chrome_options.add_experimental_option(
